85c993b88566b28e31654a8f3e908889ae710dfd
Kwang1402 <hongquang2k4@gmail.com>
2023-12-11 12:56:41 +0700

    feat(app.py): add email and reconfirm_password for register

--- user_service/app.py
+++ user_service/app.py
@@ -1,25 +1,44 @@
 from flask import Flask, request, jsonify
+from marshmallow import Schema, fields, validate
 
 app = Flask(__name__)
 
 users = []
 
+class RegistrationSchema(Schema):
+    username = fields.String(required=True)
+    email = fields.Email(required=True)
+    password = fields.String(required=True)
+    reconfirm_password = fields.String(required=True)
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
 
-    username = data.get('username')
-    password = data.get('password')
-
-    if not username or not password:
-        return jsonify({'error': 'Missing username or password'}), 400
-
-    existing_user = next((user for user in users if user['username'] == username), None)
+    try:
+        registration_data = RegistrationSchema().load(data)
+    except Exception as e:
+        return  jsonify({'error': str(e)}), 400
+    
+    existing_user = next((user for user in users if user['username'] == registration_data['username']), None)
 
     if existing_user:
         return jsonify({'error': 'Username already taken'}), 400
 
-    new_user = {'username': username, 'password': password}
+    existing_email = next((user for user in users if user['email'] == registration_data['email']), None)
+
+    if existing_email:
+        return jsonify({'error': 'Email already used'}), 400
+
+    if(registration_data['password'] != registration_data['reconfirm_password']):
+        return jsonify({'error': 'Passwords do not match'}), 400
+    
+    new_user = {
+        'username': registration_data['username'], 
+        'password': registration_data['password'], 
+        'email': registration_data['email']
+    }
+    
     users.append(new_user)
 
     return jsonify({'message': 'User successfully registered'}), 201
